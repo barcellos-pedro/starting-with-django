670ddbc4b4b0370119ab9673dc58c2f7e075a567
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2023-07-09 11:02:48 -0300

    Add comments

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,7 +5,10 @@ from . import views
 # /polls/
 urlpatterns = [
     path("", views.index, name="index"),
+    # /polls/1/
     path("<int:question_id>/", views.detail, name="detail"),
+    # /polls/1/results/
     path("<int:question_id>/results/", views.results, name="results"),
+    # /polls/1/vote/
     path("<int:question_id>/vote/", views.vote, name="vote")
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,36 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 from .models import Question
 
 
 def index(request):
     """
-    Show first 5 questions on home
+    Question index page - displays the latest few questions.
     """
-
     data = Question.objects.order_by("-pub_date")
-
-    # show on console joined list
-    # questions = ", ".join(list(map(lambda q: q.question_text, data[:5])))
-    # pritn(questions)
-
     context = {"questions": data[:5]}
-
-    # Longer way to render
-    # template = loader.get_template("polls/index.html")
-    # return HttpResponse(template.render(context, request))
-
     return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
+    """
+    Question detail page - displays a question text, with no results but with a form to vote.
+    """
     return HttpResponse(f"You're looking at question {question_id}")
 
 
 def results(request, question_id):
+    """
+    Question results page - displays results for a particular question.
+    """
     return HttpResponse(f"You're looking at the results of question {question_id}")
 
 
 def vote(request, question_id):
+    """
+    Vote action - handles voting for a particular choice in a particular question.
+    """
     return HttpResponse(f"You're voting on question {question_id}")
